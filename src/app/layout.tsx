819592bb77f7256b78b6202fdfd95b367c40b0cd
William Alvarez <walvarez1@my.bcit.ca>
2026-02-18 01:18:29 -0800

    vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
    variable: "--font-geist-sans",
@@ -54,7 +55,10 @@ export default function RootLayout({
 }>) {
    return (
       <html lang="en">
-         <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+         <body className={`${geistSans.variable} ${geistMono.variable}`}>
+            {children}
+            <SpeedInsights />
+         </body>
       </html>
    );
 }
